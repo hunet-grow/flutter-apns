92dae7794db21b83f0b550c4ca957117a20cf083
jsj1444hunet <164836180+jsj1444hunet@users.noreply.github.com>
2024-06-25 09:44:25 +0900

    Update flutter_apns_only.dart
    fix: 알림권한설정 비동기처리

--- flutter_apns_only/lib/flutter_apns_only.dart
+++ flutter_apns_only/lib/flutter_apns_only.dart
@@ -25,9 +25,10 @@ class ApnsPushConnectorOnly {
   ApnsMessageHandler? _onLaunch;
   ApnsMessageHandler? _onResume;
 
-  void requestNotificationPermissions(
-      [IosNotificationSettings iosSettings = const IosNotificationSettings()]) {
-    _channel.invokeMethod(
+  Future<void> requestNotificationPermissions(
+      [IosNotificationSettings iosSettings =
+          const IosNotificationSettings()]) async {
+    await _channel.invokeMethod(
         'requestNotificationPermissions', iosSettings.toMap());
   }
 
